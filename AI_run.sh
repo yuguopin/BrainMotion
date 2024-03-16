67195db16a1df0316189676ba9fa14908ec96fbf
yuguopin <1528048607@qq.com>
2024-03-16 11:28:31 +0800

    modify lr

--- AI_run.sh
+++ AI_run.sh
@@ -1,11 +1,11 @@
-source activate
-conda activate torchGPU
+# source activate
+# conda activate torchGPU
 
 for model in 'CNN' 'CNNPLus' 'BrainEmotion'; do
     for lr in 0.1 0.001 0.0005 0.0001; do
-        for epoch in 10 20 30 40; do
+        for epoch in 30 50; do
             for dataset in 'CIFAR100' 'FashionMNIST'; do
-                python main.py --model $model --lr $lr --epoch $epoch --dataset $dataset
+                python main.py --model $model --lr $lr --epoch $epoch --dataset $dataset --device cuda:4
             done
         done
     done
